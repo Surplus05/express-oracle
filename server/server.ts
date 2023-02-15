07b1f3f2ee6806f8729b401f7ebb80f539d230cb
Surplus05 <104773096+Surplus05@users.noreply.github.com>
2023-02-16 04:03:36 +0900

    Update server.ts

--- server/server.ts
+++ server/server.ts
@@ -103,9 +103,3 @@ app
       signUp(request.body.data, response, oracledb, dbconfig);
     else response.sendStatus(400);
   });
-
-// 로그인 은 유저 정보를 받아오는 거니 GET아닌가 싶었으나 실제 로그인 구현시에는 세션이나 쿠키를 Response하는데, 이 세션, 쿠키들 (Resource) 를 생성한다는 의미로 POST 를 사용하면 Restful 하다고 한다.
-
-// Delete Post의 경우, postId만 queryParam으로 보내면 다른 사용자들도 postId만 알면 삭제할수 있으므로 post를 사용함
-
-// 실제 서버에서는 Get 은 가져온다는 개념이고, Post 는 수행한다는 개념으로 두개만 사용하는 경우가 많다고 한다.
